2b8b0a2040883cd2fcfb6c20006863ba3f1c6b60
Philipp Resch <philipp.resch@bestplaces.at>
2013-01-27 19:04:19 +0100

    added linebreaks for better readability2

--- pyowm.py
+++ pyowm.py
@@ -51,7 +51,8 @@ class OpenWeatherMapApi(object):
         return self.get_station_list_from_dictionary(stations)
     
     def get_city_weather_by_bounding_box(self, bbox):
-        ''' return a list of cities whose geographic coordinates lay within the rectangle bbox'''
+        ''' return a list of cities whose geographic coordinates lay 
+        within the rectangle bbox'''
         
         query_part = 'city?bbox=%s&cluster=yes' % bbox
         query_url = self.openweathermap_find_url + query_part
@@ -61,7 +62,8 @@ class OpenWeatherMapApi(object):
         return self.get_city_list_from_dictionary(cities)
     
     def get_station_by_coordinates_radius(self, latitude, longitude, radius):
-        ''' return a list of stations whose coordinates lay within a circle, circle is defined by a center point and a radius'''
+        ''' return a list of stations whose coordinates lay within a circle, 
+        circle is defined by a center point and a radius'''
         
         query_part = 'station?lat=%f&lon=%f&radius=%d' % (latitude, longitude, radius)
         query_url = self.openweathermap_find_url + query_part
@@ -71,7 +73,8 @@ class OpenWeatherMapApi(object):
         return self.get_station_list_from_dictionary(stations)
     
     def get_city_weather_by_coordinates_radius(self, latitude, longitude, radius):
-        ''' return a list of cities whose coordinates lay witin a circle, circle is defined by a center point and a radius'''
+        ''' return a list of cities whose coordinates lay witin a circle, 
+        circle is defined by a center point and a radius'''
         
         query_part = 'city?lat=%f&lon=%f&radius=%d' % (latitude, longitude, radius)
         query_url = self.openweathermap_find_url + query_part
@@ -103,16 +106,19 @@ class OpenWeatherMapApi(object):
         return self.get_city_list_from_dictionary(cities)
     
     def get_city_weater_by_id(self, identifier):
-        ''' return the current weather in a concrete chosen city where you know the city id.'''
+        ''' return the current weather in a concrete chosen city where you know 
+        the city id.'''
         
         query_part = '%d' % (identifier)
         query_url = self.openweathermap_city_url + query_part
         
         city = self.open_url_decode_simplejson(query_url)
         
-        return City(get_list_item(city, 'id'), get_list_item(city, 'name'), get_list_item(city, 'coord'), get_list_item(city, 'distance'), 
-                        get_list_item(city, 'main'), get_list_item(city, 'dt'), get_list_item(city, 'wind'), get_list_item(city, 'clouds'), 
-                        get_list_item(city, 'weather'), get_list_item(city, 'sys'))
+        return City(get_list_item(city, 'id'), get_list_item(city, 'name'), 
+                    get_list_item(city, 'coord'), get_list_item(city, 'distance'), 
+                    get_list_item(city, 'main'), get_list_item(city, 'dt'), 
+                    get_list_item(city, 'wind'), get_list_item(city, 'clouds'), 
+                    get_list_item(city, 'weather'), get_list_item(city, 'sys'))
     
     def get_forecast_by_id(self, identifier):
         ''' return the forecast of the city for the next 7 days by given id'''
@@ -136,7 +142,8 @@ class OpenWeatherMapApi(object):
         self.get_forecast_list_from_dictionary(forecast)
     
     def get_daily_forecast(self, identifier):
-        ''' return the forecast of the city for the next 7 days in a compact format by given id'''
+        ''' return the forecast of the city for the next 7 days in a compact 
+        format by given id'''
         
         query_part = '%d?mode=daily_compact' % (identifier)
         query_url = self.openweathermap_forecast_url + query_part
@@ -162,10 +169,11 @@ class OpenWeatherMapApi(object):
         station_list = []
         
         for s_station in dictionary['list']:
-            station = Station(get_list_item(s_station, 'id'), get_list_item(s_station, 'dt'), get_list_item(s_station, 'name'), 
-                              get_list_item(s_station, 'type'), get_list_item(s_station, 'coord'), get_list_item(s_station, 'distance'), 
-                              get_list_item(s_station, 'main'), get_list_item(s_station, 'wind'), get_list_item(s_station, 'clouds'), 
-                              get_list_item(s_station, 'rain'))
+            station = Station(get_list_item(s_station, 'id'), get_list_item(s_station, 'dt'), 
+                              get_list_item(s_station, 'name'), get_list_item(s_station, 'type'), 
+                              get_list_item(s_station, 'coord'), get_list_item(s_station, 'distance'), 
+                              get_list_item(s_station, 'main'), get_list_item(s_station, 'wind'), 
+                              get_list_item(s_station, 'clouds'), get_list_item(s_station, 'rain'))
             station_list.append(station)
             
         return station_list
@@ -175,8 +183,10 @@ class OpenWeatherMapApi(object):
         city_list = []
         
         for s_city in dictionary['list']:
-            city = City(get_list_item(s_city, 'id'), get_list_item(s_city, 'name'), get_list_item(s_city, 'coord'), get_list_item(s_city, 'distance'), 
-                        get_list_item(s_city, 'main'), get_list_item(s_city, 'dt'), get_list_item(s_city, 'wind'), get_list_item(s_city, 'clouds'), 
+            city = City(get_list_item(s_city, 'id'), get_list_item(s_city, 'name'),
+                        get_list_item(s_city, 'coord'), get_list_item(s_city, 'distance'), 
+                        get_list_item(s_city, 'main'), get_list_item(s_city, 'dt'), 
+                        get_list_item(s_city, 'wind'), get_list_item(s_city, 'clouds'), 
                         get_list_item(s_city, 'weather'), get_list_item(s_city, 'sys'))
             
             city_list.append(city)
@@ -189,11 +199,13 @@ class OpenWeatherMapApi(object):
         forecast_items = []
          
         for item in get_list_item(dictionary, 'list'):
-            forecast_item = ForecastItem(get_list_item(item, 'clouds'), get_list_item(item, 'snow'), get_list_item(item, 'dt_txt'), 
-                                         get_list_item(item, 'weather'), get_list_item(item, 'main'), get_list_item(item, 'wind'))
+            forecast_item = ForecastItem(get_list_item(item, 'clouds'), get_list_item(item, 'snow'), 
+                                         get_list_item(item, 'dt_txt'), get_list_item(item, 'weather'), 
+                                         get_list_item(item, 'main'), get_list_item(item, 'wind'))
             forecast_items.append(forecast_item)
 
-        return Forecast(get_list_item(dict, 'id'), get_list_item(dict, 'city'), get_list_item(dict, 'url'), forecast_items)
+        return Forecast(get_list_item(dict, 'id'), get_list_item(dict, 'city'), 
+                        get_list_item(dict, 'url'), forecast_items)
         
     def get_compact_forecast_from_dictionary(self, dictionary):
         ''' returns a Forecast_compact object containing ForecastItem_compact objects'''
@@ -201,10 +213,11 @@ class OpenWeatherMapApi(object):
         forecast_compact_items = []
         
         for item in get_list_item(dictionary, 'list'):
-            compact_forecast_item = ForecastItem_compact(get_list_item(item, 'dt'), get_list_item(item, 'temp'), get_list_item(item, 'night'), 
-                                                         get_list_item(item, 'eve'), get_list_item(item, 'morn'), get_list_item(item, 'pressure'), 
-                                                         get_list_item(item, 'humidity'), get_list_item(item, 'weather'), get_list_item(item, 'speed'), 
-                                                         get_list_item(item, 'deg'))
+            compact_forecast_item = ForecastItem_compact(get_list_item(item, 'dt'), get_list_item(item, 'temp'), 
+                                                         get_list_item(item, 'night'), get_list_item(item, 'eve'), 
+                                                         get_list_item(item, 'morn'), get_list_item(item, 'pressure'), 
+                                                         get_list_item(item, 'humidity'), get_list_item(item, 'weather'), 
+                                                         get_list_item(item, 'speed'),                                                      get_list_item(item, 'deg'))
             forecast_compact_items.append(compact_forecast_item)
             
         return Forecast_compact(forecast_compact_items)
